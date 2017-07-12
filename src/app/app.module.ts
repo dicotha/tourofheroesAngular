e1861ebae2046d5cf9c05a02a251b066e9616cae
waldir bertuqui <wbn_k@hotmail.com>
2017-07-12 16:25:08 -0300

    adding routes on project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import {HeroDetailComponent} from './hero-detail.component';
-
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: 'heroes',
+       component: HeroesComponent},
+      {path:'dashboard',
+       component: DashboardComponent},
+      {path: '',
+       redirectTo: '/dashboard',
+       pathMatch: 'full'} 
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
